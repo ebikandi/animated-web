2e0f3817ffd160ca75b33414b16fdff96f0ef99c
ebikandi <ebikandi@gmail.com>
2022-08-04 10:40:04 +0200

    install reanimated

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
 
 export default function App() {
+  const offset = useSharedValue(0);
+
+  // const [animation, setAnimation] = React.useState(true);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    };
+  }, [offset /*, animation*/]);
+
+  console.log({ animatedStyles });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Animated.View
+        style={[
+          { width: 50, height: 50, backgroundColor: "lightblue" },
+          animatedStyles,
+        ]}
+      />
+      <Button
+        title="Move button"
+        onPress={() => {
+          // setAnimation(prevAnim => !prevAnim);
+          offset.value = withSpring(Math.random());
+
+          // setOffset(withSpring(Math.random()));
+          // offset.value = withSpring(Math.random());
+        }}
+      >
+        <Text>Move button</Text>
+      </Button>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
